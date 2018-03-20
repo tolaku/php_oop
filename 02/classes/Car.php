28206ed1a4ab04c2ebb72b9db74240cb55d0924a
Anatol <tola_k@mail.ru>
2018-03-20 15:21:02 +0300

    2 lesson

--- 02/classes/Car.php
+++ 02/classes/Car.php
@@ -7,4 +7,7 @@ class Car
   public $wheels = 4;
   public $speed = 180;
   var $brand;
+  
+  public $path = __DIR__ .'/test';
+  public $var = 1+2;
 }
--- 02/index.php
+++ 02/index.php
@@ -1,5 +1,38 @@
 <?php
+/**
+ * включаем показ ошибок
+ */
+error_reporting();
+
 require_once 'classes/Car.php';
+
+function debug($var){
+    echo '<pre>'. print_r($var, 1) .'</pre>';
+}
 
 // создаем объект
 $car1 = new Car();
+debug($car1); 
+debug($car2); 
+
+// обращаемся к свойству класса
+$car1->color = 'Черный';
+$car1->brand = 'Вольво';
+$car1->year = '2012';
+
+
+// обращаемся к свойству класса
+$car1->color = 'Белый';
+$car1->brand = 'БМВ';
+$car1->speed = '200';
+
+//debug($car1);
+debug($car2);
+
+echo "<h3>О моем авто:</h3>
+Марка: {$car1->brand}<br>
+Цвет: {$car1->color}<br>
+Кол-во колес: {$car1->wheels}<br>
+Год выпуска: {$car1->year}<br>
+Скорость: {$car1->speed}<br>
+";
